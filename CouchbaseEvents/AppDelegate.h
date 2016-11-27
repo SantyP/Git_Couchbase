5f81eb3fc5d3f1d12065bb77fffb90037afe651e
Santosh Vishwakarma <vish_santosh85@yahoo.co.in>
2016-11-27 11:33:45 +0530

    Initial Commit

---
+++ CouchbaseEvents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CouchbaseEvents
+//
+//  Created by Trainer on 16/09/16.
+//  Copyright © 2016 Couchbase Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CouchbaseEvents/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CouchbaseEvents
+//
+//  Created by Trainer on 16/09/16.
+//  Copyright © 2016 Couchbase Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
